bb879c1b3439a50fb393be9995669ff5aede44cf
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-04-28 18:11:52 +0530

    Middle of the 2nd day

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
 # hibernateORM
-WEB2 Inclass Project on 04-19-2023
+WEB2 Inclass Project on 2023/(04/19|04/24|04-26|04-27)
 
 04-19:
-  Project created without web
-  hibernate configeration with hibernate.properties files discussed.
+
+1.Project created without web
+
+2.hibernate configuration with hibernate.properties files discussed.
+
+
+04-24:
+
+There are 3 ways to configure hibernate:
+
+1.Using hibernate.properties file:
+
+            InputStream inputStream = Test.class.getClassLoader().getResourceAsStream("res/hibernate-dev.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            configuration.setProperties(properties);
+    
+2.Using hibernate.cfg.xml file:
+
+This should be located at the root of the classpath.
+
+We can make custom configuration xml file. 
+
+It should be passed as a parameter in the configuration file
+            
+            configuration.configure("hibernate-dev.cfg.xml");
+
+3.Programmetic Configuration
+
+(Without using any configuration file but passing configuration parameters in the code itself)
+
+        Properties properties = new Properties();
+    
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3307/orm_db");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        
+        configuration.setProperties(properties);
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,14 +1,48 @@
 import com.jiat.orm.entity.Student;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Configuration configuration = new Configuration();
+
+////        Using the following line to load the hibernate.cfg.xml file
+//        configuration.configure("hibernate-dev.cfg.xml");
+
+
+//        //Load the properties file
+//
+//        InputStream inputStream = Test.class.getClassLoader().getResourceAsStream("res/hibernate-dev.properties");
+//        Properties properties = new Properties();
+//        properties.load(inputStream);
+//
+//        configuration.setProperties(properties);
+
+//        Programmetic configuration
+        Properties properties = new Properties();
+
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3307/orm_db");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+
+        configuration.setProperties(properties);
+
 //        Need to add the following snippet to auto generate the tables in db as in JPA
         configuration.addAnnotatedClass(Student.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
+//        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        SessionFactory factory = configuration
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
 
     }
 }
